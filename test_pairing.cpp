33fd2212774e0cc758251d5db96f8df977531f93
bwv1011 <kai.kinoshita.0129@gmail.com>
2020-12-07 16:22:06 +0900

    Add files via upload

---
+++ test_pairing.cpp
@@ -0,0 +1,39 @@
+#include <libff/algebra/curves/alt_bn128/alt_bn128_pp.hpp>
+#include <iostream>
+#include <cassert>
+
+using namespace libff;
+
+template<typename ppT>
+void test_pairing(){
+    ppT::init_public_params();
+
+    // G1 x G2 -> GT
+
+    G1<ppT> P;
+    G2<ppT> Q;
+
+
+    P = (Fr<ppT>::random_element()) * G1<ppT>::one();
+    Q = (Fr<ppT>::random_element()) * G2<ppT>::one();
+
+    Fr<ppT> s = Fr<ppT>::random_element();
+    
+    G1<ppT> sP = s*P;
+    G2<ppT> sQ = s*Q;
+
+    GT<ppT> ans1,ans2;
+    ans1 = ppT::reduced_pairing(sP,Q);
+    ans2 = ppT::reduced_pairing(P,sQ);
+
+    assert(ans1 == ans2);
+
+    ans1.print();
+
+}
+
+int main(void)
+{
+    test_pairing<alt_bn128_pp>();
+    return 0;
+}
