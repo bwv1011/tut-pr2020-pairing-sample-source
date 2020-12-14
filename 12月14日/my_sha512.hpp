1faf49b6715ea0220d4aedb6e5e87cc9fe73cb07
kinoshita0129 <kai.kinoshita.0129@gmail.com>
2020-12-14 16:00:51 +0900

    Update

--- 12月14日/my_sha512.hpp
+++ 12月14日/my_sha512.hpp
@@ -3,4 +3,7 @@
 template<typename FieldT>
 FieldT my_SHA512_rng(const uint64_t idx);
 
+void my_SHA512(unsigned char *out, const unsigned char *in, int _len);
+
+
 #include "my_sha512.cpp"
--- 12月14日/test_sha512.cpp
+++ 12月14日/test_sha512.cpp
@@ -6,15 +6,38 @@
 using namespace libff;
 using namespace std;
 
+#define HASH_SIZE SHA512_DIGEST_LENGTH
+
+void print_hex_array(unsigned char *buf, int buf_len, const char *print_prefix=NULL){
+    printf(print_prefix);
+    for(int i=0; i<buf_len; i++){
+        if(i%16 == 0)
+            printf("\n\t");
+        printf("%02x ",buf[i]);
+    }
+    printf("\n\n");
+}
+
 template<typename ppT>
 void test_sha512(){
     ppT::init_public_params();
 
-    uint64_t idx = 1234;
+    unsigned char hash_input_1[HASH_SIZE] = "ABC";
+    unsigned char hash_input_2[HASH_SIZE] = "ABD";
+    int hash_input_len = 3;
+    
+    unsigned char hash_value_1[HASH_SIZE] = {};
+    unsigned char hash_value_2[HASH_SIZE] = {};
 
-    Fr<ppT> field_element = my_SHA512_rng<Fr<ppT>>(idx);
-    field_element.print();
+    print_hex_array(hash_input_1, HASH_SIZE, "input_1: \n");
+    print_hex_array(hash_input_2, HASH_SIZE, "input_2: \n");
 
+
+    my_SHA512(hash_value_1, hash_input_1, hash_input_len);
+    my_SHA512(hash_value_2, hash_input_2, hash_input_len);
+
+    print_hex_array(hash_value_1, HASH_SIZE, "out_1: \n");
+    print_hex_array(hash_value_2, HASH_SIZE, "out_2: \n");
     return;
 }
 
